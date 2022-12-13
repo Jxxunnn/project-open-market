f1c34f740efbdbf30f24f92333d367c8e66f7012
Jxxunnn <roof2586@naver.com>
2022-12-13 23:59:15 +0900

    feat: 찜하기 버튼 기능 구현하였음

--- src/components/ProductDetail/Card.js
+++ src/components/ProductDetail/Card.js
@@ -1,11 +1,17 @@
 import { API_END_POINT } from "../../utils/api.js";
+import {
+  getLocalStorageItemList,
+  setLocalStorageItemList,
+} from "../../utils/handleLocalStorageItemList.js";
 import Info from "./Info.js";
 export default function Card({ $target, initialState }) {
   const $card = document.createElement("div");
   $card.className = "flex flex-col gap-5 md:flex-row";
 
   this.state = initialState;
-
+  if (getLocalStorageItemList("wished").includes(this.state.productId)) {
+    this.state.wished = true;
+  }
   this.setState = (nextState) => {
     this.state = nextState;
     this.render();
@@ -99,7 +105,7 @@ export default function Card({ $target, initialState }) {
         class="md:flex-[5_1_0%] flex-[3_1_0%] text-center text-white font-bold text-base md:text-xl py-2 md:py-4 bg-purple-600 rounded-lg"
         >바로 구매</a
       ><button
-        class="flex items-center justify-center flex-1 border-2 rounded-lg"
+        class="store flex items-center justify-center flex-1 border-2 rounded-lg"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -107,7 +113,7 @@ export default function Card({ $target, initialState }) {
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="purple"
-          class="w-6 h-6"
+          class="store w-6 h-6"
           aria-label="장바구니로 이동"
         >
           <path
@@ -117,22 +123,35 @@ export default function Card({ $target, initialState }) {
           />
         </svg></button
       ><button
-        class="flex items-center justify-center flex-1 border-2 rounded-lg"
+        class="wished flex items-center justify-center flex-1 border-2 rounded-lg"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6 opacity-30"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-          />
-        </svg>
+       ${
+         this.state.wished
+           ? `<svg
+           xmlns="http://www.w3.org/2000/svg"
+           viewBox="0 0 24 24"
+           fill="red"
+           class="wished w-6 h-6"
+         >
+           <path
+             d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"
+           />
+         </svg>`
+           : `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="wished w-6 h-6 opacity-30"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+          </svg>`
+       }
       </button>
     </div>
   </div>
@@ -141,5 +160,17 @@ export default function Card({ $target, initialState }) {
 
   this.render();
 
-  $card.addEventListener("click", (e) => {});
+  $card.addEventListener("click", (e) => {
+    if (e.target.classList.contains("wished")) {
+      this.setState({ ...this.state, wished: !this.state.wished });
+      setLocalStorageItemList(
+        "wished",
+        this.state.productId,
+        this.state.wished
+      );
+    }
+    if (e.target.classList.contains("store")) {
+      console.log(1);
+    }
+  });
 }
