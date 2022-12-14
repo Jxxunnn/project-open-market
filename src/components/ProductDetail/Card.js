6776989ea732bd4e566dcea9aa2bdc92f1bdb8e5
Jxxunnn <roof2586@naver.com>
2022-12-15 01:19:55 +0900

    fix: 찜하기 기능 이벤트 타겟 수정

--- src/components/ProductDetail/Card.js
+++ src/components/ProductDetail/Card.js
@@ -116,13 +116,14 @@ export default function Card({ $target, initialState }) {
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke=${this.state.stored ? "purple" : "#B3B3B3"}
-          class="store w-6 h-6"
+          class="stored w-6 h-6"
           aria-label="장바구니로 이동"
         >
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
             d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+            class="stored"
           />
         </svg></button
       ><button
@@ -138,6 +139,7 @@ export default function Card({ $target, initialState }) {
          >
            <path
              d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"
+             class="wished"
            />
          </svg>`
            : `<svg
@@ -148,10 +150,11 @@ export default function Card({ $target, initialState }) {
             stroke="currentColor"
             class="wished w-6 h-6 opacity-30"
           >
-            <path
+            <path              
               stroke-linecap="round"
               stroke-linejoin="round"
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              class="wished"
             />
           </svg>`
        }
@@ -171,6 +174,7 @@ export default function Card({ $target, initialState }) {
         this.state.productId,
         this.state.wished
       );
+      console.log(1);
     }
     if (e.target.classList.contains("stored")) {
       this.setState({ ...this.state, stored: !this.state.stored });
@@ -179,6 +183,7 @@ export default function Card({ $target, initialState }) {
         this.state.productId,
         this.state.stored
       );
+      console.log(2);
     }
   });
 }
